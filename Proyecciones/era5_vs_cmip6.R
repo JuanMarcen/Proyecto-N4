2dbc621825eb85a3aa9c5836423031ad468c7799
JuanMarcen <jumarguti2002@gmail.com>
2025-08-26 10:57:08 +0200

    COMMIT FROM RSTUDIO

---
+++ Proyecciones/era5_vs_cmip6.R
@@ -0,0 +1,149 @@
+rm(list=ls())
+
+# comparison of densities of era5 and cmip6 in the whole grid
+
+#----Creation of dataframes----
+library(lubridate)
+#coordinates of grid
+coord <- readRDS("C:/Users/jumar/OneDrive/Escritorio/TFM/Datos/coord.rds")
+
+# ERA5 (already done)
+g500_era5 <- readRDS("C:/Users/jumar/OneDrive/Escritorio/TFM/Datos/g500_jja.rds")
+g500_era5[ , 2:ncol(g500_era5)] <- g500_era5[ , 2:ncol(g500_era5)] / 1000
+g700_era5 <- readRDS("C:/Users/jumar/OneDrive/Escritorio/TFM/Datos/g700_jja.rds")
+g700_era5[ , 2:ncol(g700_era5)] <- g700_era5[ , 2:ncol(g700_era5)] / 1000
+
+# CMIP6 (closest to original grid of ERA5)
+coord$Grid <- paste0(round(coord$lat),"N.",abs(round(coord$lon)),ifelse(coord$lon>-0.5,"E","W"))
+tpoints <-coord$Grid
+
+g500_cmip6 <- readRDS("C:/Users/jumar/OneDrive/Escritorio/Github/CMIP6_Iberian/Data/CMIP6/MPI-ESM1-2-HR_r1i1p1f1_zg/historical/Grid/iberian_MPI-ESM1-2-HR_r1i1p1f1_zg_500hPa_1960-2014.rds")
+g700_cmip6 <- readRDS("C:/Users/jumar/OneDrive/Escritorio/Github/CMIP6_Iberian/Data/CMIP6/MPI-ESM1-2-HR_r1i1p1f1_zg/historical/Grid/iberian_MPI-ESM1-2-HR_r1i1p1f1_zg_700hPa_1960-2014.rds")
+
+plev_cool_g500 <- 500
+plev_cool_g700 <- 700
+cmip6_vars <- grep("zg",names(g500_cmip6))
+cmip6_coor <- unlist(
+  strsplit(x = names(g500_cmip6)[cmip6_vars],
+           split = paste0("zg",plev_cool_g500,"[.]")))[seq(2,2*length(cmip6_vars),2)]
+cmip6_coor <- data.frame(lab = cmip6_coor,
+                         lat = as.numeric(NA),
+                         lon = as.numeric(NA))
+for(ii in 1:nrow(cmip6_coor)){
+  aux <- unlist(strsplit(cmip6_coor$lab[ii],"N"))
+  cmip6_coor$lat[ii] <- as.numeric(aux[1])
+  lon_sign <- substr(aux[2],nchar(aux[2]),nchar(aux[2]))
+  cmip6_coor$lon[ii] <- as.numeric(substr(aux[2],1,nchar(aux[2])-1))
+  if(substr(aux[2],nchar(aux[2]),nchar(aux[2])) == "W"){
+    cmip6_coor$lon[ii] <- cmip6_coor$lon[ii] * (-1)
+  }
+}
+
+coord_id <- coord$station
+coord_names <- coord$station
+outdf <- data.frame(id = coord_id,
+                    name = coord_names,
+                    ERA5 = paste0("zg",tpoints),
+                    GCM = 'MPI',
+                    lat = as.numeric(NA),
+                    lon = as.numeric(NA),
+                    near_g500 = rep("", length(tpoints)),
+                    near_g700 = rep("", length(tpoints)))
+
+# Compute scores
+for(pp in 1:length(tpoints)){
+  
+  # Get ERA5 coordinates
+  aux <- unlist(strsplit(tpoints[pp],"[.]"))
+  outdf$lat[pp] <- as.numeric(as.numeric(substr(aux[1],1,nchar(aux[1])-1)))
+  outdf$lon[pp] <- as.numeric(substr(aux[2],1,nchar(aux[2])-1))
+  if(substr(aux[2],nchar(aux[2]),nchar(aux[2])) == "W"){
+    outdf$lon[pp] <- outdf$lon[pp] * (-1)
+  }
+  
+  # Find nearest grid point in GMC
+  eudist <- sqrt((cmip6_coor$lat-outdf$lat[pp])**2 + (cmip6_coor$lon-outdf$lon[pp])**2)
+  near_idx <- which.min(eudist)[1]
+  outdf$near_g500[pp] <- names(g500_cmip6)[cmip6_vars[near_idx]]
+  outdf$near_g700[pp] <- names(g700_cmip6)[cmip6_vars[near_idx]]
+  
+}
+
+# cambio de unidades para que sea equivalente
+g500_cmip6[, cmip6_vars] <- g500_cmip6[, cmip6_vars] * 9.80665 / 1000
+g700_cmip6[, cmip6_vars] <- g700_cmip6[, cmip6_vars] * 9.80665 / 1000
+
+# dataframe of values CMIP
+g500_cmip6 <- g500_cmip6[, c('date', outdf$near_g500)]
+g700_cmip6 <- g700_cmip6[, c('date', outdf$near_g700)]
+
+#Reference period
+fecha_ref <- c('1981-06-01', '2010-08-31')
+
+g500_era5 <- g500_era5[which(g500_era5$Date >= fecha_ref[1] &
+                             g500_era5$Date <= fecha_ref[2] & 
+                             month(g500_era5$Date) != 5), ]
+colnames(g500_era5)[-1] <- paste0('g500.', colnames(g500_era5)[-1])
+g700_era5 <- g700_era5[which(g700_era5$Date >= fecha_ref[1] &
+                             g700_era5$Date <= fecha_ref[2] & 
+                             month(g700_era5$Date) != 5), ]
+colnames(g700_era5)[-1] <- paste0('g700.', colnames(g700_era5)[-1])
+
+g500_cmip6 <- g500_cmip6[which(g500_cmip6$date >= fecha_ref[1] &
+                               g500_cmip6$date <= fecha_ref[2] & 
+                               month(g500_cmip6$date) != 5), ]
+g700_cmip6 <- g700_cmip6[which(g700_cmip6$date >= fecha_ref[1] &
+                               g700_cmip6$date <= fecha_ref[2] & 
+                               month(g700_cmip6$date) != 5), ]
+
+
+#----ANOMALIES-----
+# harmonics (in order to calculate the anomalies)
+cs <- function(t,harmonics=1) {
+  # if(min(t) <0 | max(t) > 1){ stop(" t must be in [0,1] range")}
+  if(min(harmonics) <1){stop("harmonics > = 1")}
+  ret <- numeric(0)
+  for ( i in harmonics) {
+    ret <- cbind( ret, cos(2*pi*i*t/365), sin(2*pi*i*t/365))
+  }
+  if (missing(harmonics)) cnames <- c('c','s')
+  else {
+    cnames <- paste( c("c","s"), rep(harmonics, each = 2),sep=".")
+  }
+  colnames(ret) <- cnames
+  ret
+}
+
+l <- 152:243 # days
+harm <- cs(l, 1) # same for all
+
+l <- rep(l,times=30)
+harm <- matrix(rep(harm, times = 30), ncol = ncol(harm), byrow = FALSE)
+colnames(harm) <- c('c.1', 's.1')
+t <- year(g500_era5$Date) - 1960 + 1
+
+g500_era5 <- cbind(g500_era5, l, t, harm)
+g700_era5 <- cbind(g700_era5, l, t, harm)
+g500_cmip6 <- cbind(g500_cmip6, l, t, harm)
+g700_cmip6 <- cbind(g700_cmip6, l, t, harm)
+
+rm(list = setdiff(ls(), c('coord', 'g500_era5', 'g700_era5', 'g500_cmip6', 'g700_cmip6')))
+anomalies <- function(df){
+  for (i in 2:(ncol(df) - 4)){
+    var <- names(df)[i]
+    formula <- as.formula(paste(paste0('`', var, '`'), "~ s.1 + c.1"))
+    mod <- lm(formula, data = df)
+    
+    df[, var] <- mod$residuals
+  }
+  
+  return(df)
+}
+
+g500_era5 <- anomalies(g500_era5)
+g700_era5 <- anomalies(g700_era5)
+g500_cmip6 <- anomalies(g500_cmip6)
+g700_cmip6 <- anomalies(g700_cmip6)
+
+
+#----KS TEST----
